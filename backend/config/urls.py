f38799a9ced28a496ac45acdee9e8603ae8ed458
Praveenr-2101 <prabhapraveen000@gmail.com>
2025-08-31 17:21:19 +0530

    add gunicorn

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.http import HttpResponse
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('urls.api.urls')),
-     
+    path('', lambda request: HttpResponse("Welcome to Event Management System", status=200)), 
 ]
